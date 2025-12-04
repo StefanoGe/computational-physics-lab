d4f80f9dbd8bca51dd5cf03ede3667daa590f2f8
Stefano Ge <stefano.ge2004@gmail.com>
2025-12-04 11:40:51 +0100

    started cmake implementation

---
+++ build/Lezione18/Es18_1/CMakeFiles/gauss_legendre_weights.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/stefano/University/Fisica_Computazionale/StefanoGe/bin/gauss_legendre_weights"
+  "/home/stefano/University/Fisica_Computazionale/StefanoGe/bin/gauss_legendre_weights.pdb"
+  "CMakeFiles/gauss_legendre_weights.dir/link.d"
+  "CMakeFiles/gauss_legendre_weights.dir/gauss_legendre_weights.c.o"
+  "CMakeFiles/gauss_legendre_weights.dir/gauss_legendre_weights.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gauss_legendre_weights.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Lezione18/Es18_1/CMakeFiles/gauss_legendre_weights.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gauss_legendre_weights.
+# This may be replaced when dependencies are built.
