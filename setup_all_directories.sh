456c5198de3135c64bff29f8d7ed8b9fe9788986
Stefano Ge <stefano.ge2004@gmail.com>
2025-12-05 16:47:45 +0100

    minor edits

--- setup_all_directories.sh
+++ setup_all_directories.sh
@@ -10,17 +10,34 @@ create_dirs(){
 	mv "$path.c" $path
 }
 
+create_cmake(){
+	cfile="$1"
+	path=
+	touch "$path.CMakeLists.txt"
+	set(EXERCISE_SOURCES gauss_legendre_weights.c)
+	add_executable(gauss_legendre_weights ${EXERCISE_SOURCES})
+	target_link_libraries(gauss_legendre_weights PRIVATE cp_library m)
+	target_include_directories(gauss_legendre_weights PRIVATE ${CMAKE_SOURCE_DIR}/Libraries/header)
+
+	
+	
+}
+
+create_subdirectories(){
+	dir=$1
+	for cfile in $dir/*.c; do
+	[ -f "$cfile" ] || break
+	cfile="${cfile#*//}"
+	cfile="${cfile%.c}"
+#			echo "$cfile"
+	create_dirs $dir $cfile
+	done
+}
 
 for dir in */; do
 	if [[ $dir == Lezione* ]]
 	then
 		echo $dir
-		for cfile in $dir/*.c; do
-			[ -f "$cfile" ] || break
-			cfile="${cfile#*//}"
-			cfile="${cfile%.c}"
-#			echo "$cfile"
-			create_dirs $dir $cfile
-		done
+		create_subdirectories $dir
 	fi
 done
