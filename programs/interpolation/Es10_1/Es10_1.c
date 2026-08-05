fe7ad569f9edb09908b1fdbc8e5a0702f2321ebe
Stefano Ge <stefano.ge2004@gmail.com>
2026-08-05 11:19:53 +0200

    calculated cond nummber in exercise 10

--- programs/interpolation/Es10_1/Es10_1.c
+++ programs/interpolation/Es10_1/Es10_1.c
@@ -3,18 +3,18 @@
 #include <stdlib.h>
 #include "interpolation.h"
 
-double interpolate_cosine(int n, int k)
+
+double interpolate_cosine(int n, int k, double *cond_number)
 {
 	const int n_nodes = n+1;
 	Array x_values = arr_linspace(-1,1,n_nodes);
 	Array y_values = arr_new(n_nodes);
 	Array coeffs = arr_new(n_nodes);
-	double cond_number=0;
 	
 	for(int i=0; i<n_nodes; i++)
 		ARR(y_values,i) = cos(k*ARR(x_values,i));
 
-	if( !interp_polyn_vand_lup(&x_values, &y_values, &coeffs, &cond_number) )
+	if( !interp_polyn_vand_lup(&x_values, &y_values, &coeffs, cond_number) )
 	{
 		printf("for n = %d k = %d matrix was singular\n",n,k);
 		return INFINITY;
@@ -24,21 +24,25 @@ double interpolate_cosine(int n, int k)
 	
 	printf("For n = %d k = %d:\n",n,k);
 	arr_print_inline(&coeffs, "%5.3g",true,true);
-	printf("cond number: %lf\n\n", cond_number);
+	if(cond_number) printf("cond number: %lf\n\n", *cond_number);
 	
 	Array *to_free[]={&x_values,&y_values,&coeffs};
 	arr_free_many(to_free,3);
 	return value_at_zero;
 }
 
-int main()
-{
+void test_interpolation(){
+	
 	const int maxk = 100;
 	const int stepk = 10;
+	const int nstepk = maxk/stepk;
 	const int maxn = 40;
 	const int stepn = 4;
+	const int nstepn = maxn/stepn;
 
-	Matrix values_at_zero = mat_new( maxk/stepk, maxn/stepn);
+	Matrix values_at_zero = mat_new( nstepk, nstepn);
+	Array cond_number = arr_new( nstepn );
+	
 	
 //	eprint("%d %d", values_at_zero.nrows,values_at_zero.ncols);
 
@@ -46,13 +50,27 @@ int main()
 		for( int n = stepn; n <= maxn; n+=stepn )
 		{
 //			eprint("%d %d",k,n);
-			MAT(values_at_zero, k/stepk-1,n/stepn-1) = interpolate_cosine( n, k );
+			MAT(values_at_zero, k/stepk-1,n/stepn-1) = interpolate_cosine( n, k, &ARR(cond_number, n/stepn-1));
 		}
 	mat_print_stdout(&values_at_zero,"% .3lf",true);
-	
-	interpolate_cosine(10,1);
-	
+	puts("Logarithm of condition number of Vandermonde matrix scales with 10*n as:");
+	for(int j=0; j<nstepn; j++)
+	{
+		printf("%5g ", log(ARR(cond_number,j)));
+	}
+	puts("\n");
 	mat_free(&values_at_zero);
+	arr_free(&cond_number);
+	
+}
+
+int main()
+{
+	test_interpolation();
+
+	interpolate_cosine(10,1,NULL);
+	
+	// Remark: condition number scales exponentially!
 	
 	exit(EXIT_SUCCESS);
 }
